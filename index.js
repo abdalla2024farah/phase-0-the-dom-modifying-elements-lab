f6cd8e12ae9e601a7dd9edc8cf44a393407fb641
abdal farah <abdalla2024farah>
2024-07-13 00:00:28 +0300

    initial

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+// Remove the <main> element with id "main"
+const mainElement = document.getElementById("main");
+mainElement.remove();
+
+// Create a variable named newHeader and assign it the <h1> element with id "victory"
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+
+// Set the text content of the newHeader element
+newHeader.textContent = "YOUR-NAME is the champion";
+
+// Append the newHeader element to the document body
+document.body.appendChild(newHeader);
